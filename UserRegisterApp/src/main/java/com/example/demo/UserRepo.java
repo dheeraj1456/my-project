7271d01f2ddb1a3607a90b46d2a5a7db1d83b6a8
dheeraj1456 <23eg105j34@anurag.edu.in>
2026-04-02 20:58:40 +0530

    Removed unwanted folders

--- UserRegisterApp/src/main/java/com/example/demo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.User;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-
-    User findByEmailAndPassword(String email, String password);
-}
